c257181fe20b2339c71ff0d9a03167cf92d4432c
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-09-05 22:57:01 +0300

    ToDoApp Created

---
+++ src/components/Main/Main.style.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  main: {
+    flexDirection: 'row',
+  },
+  title: {
+    flex: 1,
+    fontSize: 32,
+    color: '#ffa500',
+    fontWeight: 'bold',
+    paddingLeft: 10,
+  },
+  todoCounter: {
+    fontSize: 32,
+    color: '#ffa500',
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+});
---
+++ src/components/ToDoCard/ToDoCard.style.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#102027',
+    padding: 10,
+  },
+  card_container: {
+    flex: 1,
+    backgroundColor: '#102027',
+  },
+  createContainer: {
+    justifyContent: 'flex-end',
+    backgroundColor: '#37474f',
+    borderRadius: 10,
+  },
+  innerContainer: {
+    margin: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  input: {
+    padding: 10,
+    color: '#808080',
+    fontSize: 16,
+  },
+  seperator: {
+    borderWidth: 1,
+    borderColor: '#78909c',
+  },
+});
